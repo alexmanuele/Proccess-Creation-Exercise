2ee4d51b415d42300e6649c97b317e87221ee4ec
ajoeman <30836347+ajoeman@users.noreply.github.com>
2018-10-11 18:34:28 -0300

    Added structure to incorporate history command. Does not work as expected though.

--- A1.c
+++ A1.c
@@ -7,10 +7,19 @@
 
 #define MAX_LINE 80
 
+struct pastArg{
+  int pid;
+  char args[MAX_LINE + 1];
+};
+  
+void addToHistory(struct pastArg history[10], char args[MAX_LINE +1], int pid);
+void printHistory(struct pastArg history[10]);
+  
 int main(void)
 {
   pid_t pid;
-  char *args[MAX_LINE/2 + 1]; //an array of strings apparently
+  char *args[MAX_LINE/2 + 1];
+  struct pastArg history[10]; 
   int should_run = 1;
   char rawInput[MAX_LINE + 1]; //for the input
   char* token;
@@ -35,6 +44,10 @@ int main(void)
     if(strcmp(args[0], "exit\n") == 0 || strcmp(args[0], "exit") == 0){
 	should_run = 0;
       }
+    //check if command was history
+    if(strcmp(args[0], "history\n") == 0 || strcmp(args[0], "history") == 0){
+      printHistory(history);
+    }
     //check if last argument was &
     if(strcmp(args[i-1], "&") != 0){
       should_wait = 1;
@@ -48,16 +61,39 @@ int main(void)
     }
     else if (pid == 0){
       //child
-	execvp(args[0], args);
-	printf("Child doing something\n");
+      addToHistory(history, rawInput, getpid());
+      execvp(args[0], args);
     }
     else{
       //parent
       if(should_wait){
 	wait(NULL);
        }
-      printf("Parent doing something\n");
     }
   }
   return 0;
+}
+void addToHistory(struct pastArg history[10], char args[MAX_LINE +1], int pid){
+  int i = 9;
+  while (i > 0){
+    history[i] = history[i-1];
+    i--;
+  }
+  struct pastArg argument;
+  argument.pid = pid;
+  strcpy(argument.args, args);
+  history[0] = argument;
+}
+
+void printHistory(struct pastArg history[10]){
+  char *prompt = {"ID\tPID\tCommand"};
+  int i = 0;
+  printf("%s\n", prompt);
+  while(i < 10){
+    if(history[i].args != NULL){
+      printf("%d\t%d\t%s\n",i+1, history[i].pid, history[i].args); 
+    }
+    else{
+      break;
+  }
 }
